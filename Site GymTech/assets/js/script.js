1ccced5391f992d6e65b43802691f7c1500eb882
Maël <119709626+maeldolium@users.noreply.github.com>
2025-02-23 18:10:58 +0100

    Add responsive design

---
+++ Site GymTech/assets/js/script.js
@@ -0,0 +1,9 @@
+// Script menu burger
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".burger");
+    const navLinks = document.querySelector(".nav-links");
+
+    burger.addEventListener("click", () => {
+        navLinks.classList.toggle("open");
+    });
+});
